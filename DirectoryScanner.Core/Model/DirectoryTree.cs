540eecf15ff5b4856e57b97480de5e68bb8d2f39
alexb <alexbondar03@gmail.com>
2022-11-06 15:35:51 +0300

    Add TaskQueue

--- DirectoryScanner.Core/Model/DirectoryTree.cs
+++ DirectoryScanner.Core/Model/DirectoryTree.cs
@@ -2,6 +2,10 @@
 
 public class DirectoryTree
 {
-    public TreeNode Node { get; set; }
+    public string Name { get; set; }
+    public string FullName { get; set; }
+    public long Size { get; set; }
+    public bool IsDirectory { get; set; }
+    
     public List<DirectoryTree> Childrens { get; set; }
 }
--- DirectoryScanner.Core/TaskQueue.cs
+++ DirectoryScanner.Core/TaskQueue.cs
@@ -1,6 +1,79 @@
-﻿namespace DirectoryScanner.Core;
+﻿using System.Collections.Concurrent;
 
-public class TaskQueue
+namespace DirectoryScanner.Core;
+
+public class TaskQueue : IDisposable
 {
+    private readonly Queue<Action?> _tasks = new();
+    private readonly List<Thread> _threads = new();
+    private readonly CancellationTokenSource _tokenSource;
 
+    public TaskQueue(ushort maxCount, CancellationTokenSource tokenSource)
+    {
+        for (var i = 0; i < maxCount; i++)
+        {
+            var thread = new Thread(ThreadFunc)
+            {
+                IsBackground = true
+            };
+
+            _threads.Add(thread);
+            thread.Start();
+        }
+        _tokenSource = tokenSource;
+    }
+
+    private void ThreadFunc()
+    {
+        while (!_tokenSource.Token.IsCancellationRequested)
+        {
+            var task = DequeueTask();
+            try
+            {
+                task?.Invoke();
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
+        }
+    }
+
+    private void EnqueueTask(Action? task)
+    {
+        lock (_tasks)
+        {
+            _tasks.Enqueue(task);
+            Monitor.Pulse(_tasks);
+        }
+    }
+
+    private Action? DequeueTask()
+    {
+        lock (_tasks)
+        {
+            while (_tasks.Count == 0)
+            {
+                Monitor.Wait(_tasks);
+            }
+            try
+            {
+                return _tasks.Dequeue();
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+
+    public void Dispose()
+    {
+        _tokenSource.Cancel();
+        Monitor.PulseAll(_tasks);
+        foreach (var thread in _threads)
+        {
+            thread.Join();
+        }
+    }
 }
